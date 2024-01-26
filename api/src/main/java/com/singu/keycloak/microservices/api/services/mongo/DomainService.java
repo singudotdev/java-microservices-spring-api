46282422c86931e8382d18861f67c1940542e031
singuuu <jota.llopis@pm.me>
2024-01-26 23:13:37 +0100

    Divide project in two and add a gateway with Consul

---
+++ api/src/main/java/com/singu/keycloak/microservices/api/services/mongo/DomainService.java
@@ -0,0 +1,38 @@
+package com.singu.keycloak.microservices.api.services.mongo;
+
+import com.singu.keycloak.microservices.api.domains.responses.DomainResponse;
+import com.singu.keycloak.microservices.api.domains.mongo.Domain;
+import com.singu.keycloak.microservices.api.domains.requests.DomainRequest;
+import com.singu.keycloak.microservices.api.mappers.DomainMapper;
+import com.singu.keycloak.microservices.api.repositories.mongo.DomainRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DomainService {
+
+    private final DomainRepository repository;
+
+    public Domain getByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public List<DomainResponse> getAll() {
+        final List<Domain> domains = repository.findAll();
+
+        List<DomainResponse> domainResponses = new ArrayList<>();
+
+        domains.forEach(domain -> domainResponses.add(DomainMapper.generateResponse(domain)));
+        return domainResponses;
+    }
+
+    public DomainResponse create(DomainRequest request) {
+        final Domain createdDomain = repository.save(DomainMapper.createDomain(request));
+
+        return DomainMapper.generateResponse(createdDomain);
+    }
+}
---
+++ api/src/test/java/com/singu/keycloak/microservices/gateway/ApiApplicationTests.java
@@ -0,0 +1,19 @@
+package com.singu.keycloak.microservices.gateway;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        assertTrue(true);
+
+    }
+
+}
